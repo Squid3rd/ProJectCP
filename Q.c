be7d808f85a78a54ac264d745e6405c4ca141a12
64070167 <64070167@it.kmitl.ac.th>
2022-04-21 03:05:39 +0700

    q02
    fix02

--- Q.c
+++ Q.c
@@ -33,19 +33,70 @@ int main(){
         }
     else if(choose == 'S'){
         system("cls");
-        printf("How to play Quiz GAME!!!\n");
-        printf("1.There are 3 modes in this game, Easy, Normal and Hard.\n2.When entering the game, there will be 10 questions per category, 4 choices each.\n3.When more than 6 correct answers or less than 4 wrong answers do a promotion Proceed to the next level starting with Easy Normal and ending with Hard.\n4.If answering 4 questions incorrectly will be downgraded.(in case of Easy, questions will be reset.)\n");
-        printf("\"Y\" Continous\n");
+        goto Rule;
+    }
+    else{
+        goto mainhome;
+    }
+   Rule:
+        system("cls");
+        printf("\n\n\t\t\t.----------------------------.\n");
+        printf("\t\t\t|  How to play Quiz GAME!!!  |\n");
+        printf("\t\t\t'----------------------------'\n\n");
+        printf("\t1.There are 3 modes in this game, Easy, Normal and Hard.\n\
+        2.When entering the game, there will be 10 questions per category, 4 choices each.\n\
+        3.When more than 6 correct answers or less than 4 wrong answers do a promotion Proceed to the next level \n\
+        starting with Easy Normal and ending with Hard.\n\
+        4.If answering 4 questions incorrectly will be downgraded.(in case of Easy, questions will be reset.)\n");
+        printf("\n\n\t\t\t\"Y\" To begin this Quiz\n");
         if (toupper(getch())=='Y')
         {
             goto Easy;
         }
-        else
+        else if(toupper(getch())=='N')
         {
             goto mainhome;
         }
+        else
+        {
+            goto Rule;
+        }
+    
+
+    Retry:
+    system("cls");
+    printf("   ______________________________\n\
+ / \\                             \\.\n\
+|   |      -----------------     |.\n\
+ \\_ |      | YOU FAIL QUIZ |     |.\n\
+    |      -----------------     |.\n\
+    |                            |.\n\
+    |                            |.\n\
+    |     Press \"Y\" to Retry     |.\n\
+    |     Press \"N\" to Menu      |.\n\
+    |                            |.\n\
+    |                            |.\n\
+    |                            |.\n\
+    |                            |.\n\
+    |                            |.\n\
+    |                            |.\n\
+    |   _________________________|___\n\
+    |  /                            /.\n\
+    \\_/____________________________/.");
+    
+    if (toupper(getch())=='Y')
+        {
+            goto Easy;
+        }
+    else if(toupper(getch())=='N')
+        {
+            goto mainhome;
+        }
+    else
+    {
+        goto Retry;
+        
     }
-   
 
     Easy:
     system("cls");
@@ -59,8 +110,11 @@ int main(){
     switch(question)
     {
         case 1:
-		printf("\n\n1.If Thailand is divided into 6 regions Which region has the most provinces?");
-		printf("\n\nA.Central\t\tB.Sourthern\n\nC.Northern\t\tD.Northeast");
+        printf("\n\n\t\t+-------------------------+\n");
+        printf("\t\t|   Your current HP: %d    |\n", hp);
+        printf("\t\t+-------------------------+\n");
+		printf("\n\n\t1.If Thailand is divided into 6 regions Which region has the most provinces?");
+		printf("\n\n\t\tA.Central\t\tB.Sourthern\n\n\t\tC.Northern\t\tD.Northeast");
 		if (toupper(getch())=='D')
 		{
 		    printf("\n\nCorrect!!!");
@@ -70,10 +124,10 @@ int main(){
 		else
 		{
 		    printf("\n\nWrong!!! Northeast\n");
-            printf("%d", hp);
+            
             hp--;
             if(hp == 0){
-        goto mainhome;
+        goto Retry;
     }
     
 		    getch();
@@ -82,8 +136,11 @@ int main(){
             
 		}
         case 2:
+        printf("\n\n\t\t+-------------------------+\n");
+        printf("\t\t|   Your current HP: %d    |\n", hp);
+        printf("\t\t+-------------------------+\n");
 		printf("\n\n2.If Thailand is divided into 6 regions Which region has the most provinces?");
-		printf("\n\nA.Central\t\tB.Sourthern\n\nC.Northern\t\tD.Northeast");
+		printf("\n\n\tA.Central\t\tB.Sourthern\n\n\tC.Northern\t\tD.Northeast");
 		if (toupper(getch())=='D')
 		{
 		    printf("\n\nCorrect!!!");
@@ -93,17 +150,20 @@ int main(){
 		else
 		{
 		    printf("\n\nWrong!!! Northeast\n");
-            printf("%d", hp);
+            
             hp--;
 		    getch();
             if(hp == 0){
-        goto mainhome;
+        goto Retry;
     }
 		    break;
 		}
         case 3:
+        printf("\n\n\t\t+-------------------------+\n");
+        printf("\t\t|   Your current HP: %d    |\n", hp);
+        printf("\t\t+-------------------------+\n");
 		printf("\n\n3.If Thailand is divided into 6 regions Which region has the most provinces?");
-		printf("\n\nA.Central\t\tB.Sourthern\n\nC.Northern\t\tD.Northeast");
+		printf("\n\n\tA.Central\t\tB.Sourthern\n\n\tC.Northern\t\tD.Northeast");
 		if (toupper(getch())=='D')
 		{
 		    printf("\n\nCorrect!!!");
@@ -113,11 +173,11 @@ int main(){
 		else
 		{
 		    printf("\n\nWrong!!! Northeast\n");
-            printf("%d", hp);
+            
             hp--;
 		    getch();
             if(hp == 0){
-        goto mainhome;
+        goto Retry;
     }
 		    break;
 		}
@@ -126,5 +186,6 @@ int main(){
 
     Normal:
     system("cls");
-    printf("hhhdfgfdgfdgfdgdfgfdgdfgdf");
+    printf("Welcome to Normal!\t");
+    printf("");
 }
